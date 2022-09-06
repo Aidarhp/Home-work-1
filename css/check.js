adfcf21926dae8d7899d7443ea250dcca50ba08b
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2022-09-06 19:52:53 +0600

    first commit

---
+++ css/check.js
@@ -0,0 +1,9 @@
+const number1 = prompt('Введите число');
+const number2 = prompt('Введите число');
+if (number1 >= number2) {
+    alert(`${number1} Больше чем ${number2}`);
+} else if (number1 <= number2 ) {
+    alert(`${number2} Больше чем ${number1}`);
+} else if (number1 = number2) {
+    alert('Равны')
+}
---
+++ css/tr.js
@@ -0,0 +1,15 @@
+let a = prompt('Введите свет для светофора')
+switch (a) {
+    case 'красный':
+        alert('Стой')
+        break;
+    case 'зелёный':
+        alert('проходи')
+        break;
+    case 'желтый':
+        alert('Приготовься')
+        break;
+    default:
+        alert('Не понятно')
+
+}
